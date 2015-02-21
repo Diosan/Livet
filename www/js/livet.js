06c3b36aa6928a83a8c2a8ed11a3d08bf9e7fa1a
Dion Santana <dion.santana@gmail.com>
2015-02-21 14:34:24 -0400

    emi working panel

--- www/js/livet.js
+++ www/js/livet.js
@@ -1,2 +1,11 @@
 
+$(function() {
+    
+	$('#profile_save').click( function() {
+		//alert('She bun d man baskside yes');
+		// Store
+		localStorage.setItem("Name", $('#profile_name').val());	
+	});
+
+});
 
