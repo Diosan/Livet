f9fe276e09231091e1a456b773245611fe0127a3
Dion Santana <dion.santana@gmail.com>
2015-02-23 16:00:44 -0400

    reading form

--- www/js/livet.js
+++ www/js/livet.js
@@ -1,26 +1,75 @@
 
+var date = new Date();
+var d = date.getDate();
+var m = date.getMonth();
+var y = date.getFullYear();
+
+
+
+// Create an array that will contain the events
+var events = [];
+
+
 $(function() {
 	
 	setProfile();
 	setupCalendar();
     
 	$('#profile_save').click( function() {
-		//alert('She bun d man baskside yes');
+		
 		// Store
 		saveProfile();
 		setProfile();	
 	});
+	
+	$('#reading_save').click( function() {
+		//var today = new Date();
+		//alert(today.getUTCMonth());
+		var date = new Date();
+		var d = date.getDate();
+		var m = date.getMonth();
+		var y = date.getFullYear();
+		var type = $('#reading_type').val();
+		var value = $('#reading_value').val();
+		events.push({ type : value, "begin": new Date(y, m, d), "end": new Date(y, m, d + 1) });
+		$('#calendar').trigger('refresh');
+		//, "begin": new Date(y, m, d + 3), "end": new Date(y, m, d + 4)
+	});
+	
+		
 
 });
 
+
+
 function saveProfile() {
-	localStorage.setItem("Name", $('#profile_name').val());
-	localStorage.setItem("Age", $('#profile_age').val() + ' years' );
+	localStorage.setItem("name", $('#profile_name').val());
+	localStorage.setItem("age", $('#profile_age').val() + ' years' );
 }
 
 function setProfile() {
-	$('#profileName').html(localStorage.getItem("Name"));
-	$('#profileAge').html(localStorage.getItem("Age"));
+	
+	setProfilefield('name');
+	setProfilefield('age');
+	
+}
+
+function setProfilefield(fieldName) {
+	
+	if (localStorage.getItem(fieldName) === null) {
+		//alert('field IS empty');
+		$('#profile' + fieldName).html('');
+		//$('#profile_' + fieldName).val('');
+	} else {
+		//alert('field is NOT empty');
+		$('#profile' + fieldName).html(localStorage.getItem(fieldName));
+		//$('#profile_' + fieldName).val(localStorage.getItem(fieldName));
+		//if ($('#profile_' + fieldName).attr('type') == 'range') {
+			//$('#profile_' + fieldName).val(localStorage.getItem(fieldName)).slider('disabled', true).slider('refresh');
+			//$('#profile_' + fieldName).val(localStorage.getItem(fieldName)).slider("refresh");
+		//}
+	}
+	
 }
 
 
